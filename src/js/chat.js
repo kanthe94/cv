c1b4821c73ab338944a80a37827ee016e2695700
techwriter94 <ashyboy777@gmail.com>
2021-04-12 15:46:22 +0530

    Chat JS added

---
+++ src/js/chat.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60741ce6f7ce182709397590/1f32p1p9b';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
